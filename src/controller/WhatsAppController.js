df581e746ae810f0539abd1a02c9f768cc85f84e
lukkas-lukkas <66659328+lukkas-lukkas@users.noreply.github.com>
2021-03-23 20:42:31 -0300

    Evento de click em um contato

--- src/controller/WhatsAppController.js
+++ src/controller/WhatsAppController.js
@@ -517,6 +517,20 @@ export class WhatsAppController{
                     </div>
                 `;
 
+                div.on('click', event=>{
+                    this.el.activeName.innerHTML = contact.name;
+                    this.el.activeStatus.innerHTML = contact.status;
+                    if(contact.photo){
+                        let img = this.el.activePhoto;
+                        img.src = contact.photo;
+                        img.show();
+                    }
+                    this.el.home.hide();
+                    this.el.main.css({
+                        display: 'flex'
+                    })
+                });
+
                 if(contact.photo){
                     let img = div.querySelector('.photo');
                     img.src = contact.photo;
