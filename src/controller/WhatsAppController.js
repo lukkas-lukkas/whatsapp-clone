de160cb3f4afc67eff6bbb5cfc28786081773c2b
lukkas-lukkas <66659328+lukkas-lukkas@users.noreply.github.com>
2021-03-24 19:58:15 -0300

    Lendo as mensagens de um chat em Tempo Real (Realtime)

--- src/controller/WhatsAppController.js
+++ src/controller/WhatsAppController.js
@@ -14,10 +14,6 @@ export class WhatsAppController{
         this.elementsPrototype();
         this.loadElements();
         this.initEvents();
-
-        /*this.el.appContent.css({
-            display: 'flex'
-        });*/
     }
 
     elementsPrototype(){
@@ -552,6 +548,11 @@ export class WhatsAppController{
     }
 
     setActiveChat(contact){
+
+        if(this._contactActive){
+            Message.getRef(this._contactActive.chatId).onSnapshot(()=>{});
+        }
+
         this._contactActive = contact;
 
         this.el.activeName.innerHTML = contact.name;
@@ -565,5 +566,28 @@ export class WhatsAppController{
         this.el.main.css({
             display: 'flex'
                     })
+                    
+        Message.getRef(this._contactActive.chatId).orderBy('timeStamp').onSnapshot(docs=>{
+            this.el.panelMessagesContainer.innerHTML = '';
+
+            docs.forEach(doc=>{
+                let data = doc.data();
+                data.id = doc.id;
+                
+                if(!this.el.panelMessagesContainer.querySelector(`#_${data.id}`)){
+                    let message = new Message;
+    
+                    message.fromJSON(data);
+                
+                    let me = (data.from === this._user.email);
+                
+                    let view = message.getViewElement(me);
+
+                    this.el.panelMessagesContainer.appendChild(view);
+                }
+                
+            })
+        })
+        
     }
 }
