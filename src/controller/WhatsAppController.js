d50915947203f7180d64b884d090ddb598c38122
lukkas-lukkas <66659328+lukkas-lukkas@users.noreply.github.com>
2021-03-25 19:38:18 -0300

    Atualizando status de mensagem (wait, sent e read)

--- src/controller/WhatsAppController.js
+++ src/controller/WhatsAppController.js
@@ -589,19 +589,31 @@ export class WhatsAppController{
                 let data = doc.data();
                 data.id = doc.id;
                 
-                if(!this.el.panelMessagesContainer.querySelector(`#_${data.id}`)){                    
-
-                    let message = new Message;
-    
-                    message.fromJSON(data);
+                let message = new Message;
                 
-                    let me = (data.from === this._user.email);
+                message.fromJSON(data);
+
+                let me = (data.from === this._user.email);
+
+                if(!this.el.panelMessagesContainer.querySelector(`#_${data.id}`)){
+
+                    if(!me){
+                        doc.ref.set({
+                            status: 'read'
+                        }, {
+                            merge: true
+                        })
+                    }
                 
                     let view = message.getViewElement(me);
 
                     this.el.panelMessagesContainer.appendChild(view);
 
                     
+                } else if(me) {
+                    let msgEl = this.el.panelMessagesContainer.querySelector(`#_${data.id}`);
+
+                    msgEl.querySelector('.message-status').innerHTML = message.getStatusViewElement().outerHTML;
                 }
                 
             });
